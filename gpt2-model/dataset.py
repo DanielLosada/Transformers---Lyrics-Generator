a2cb15649751481c36f4ad883691546e1ae22f14
paurosci <pau.rosell@wallbox.com>
2023-07-01 23:32:02 +0200

    includes 79-genre dataset support

--- gpt2-model/dataset.py
+++ gpt2-model/dataset.py
@@ -1,20 +1,21 @@
 import os
 import re
 import zipfile
-import csv
-from datasets import load_dataset, concatenate_datasets
+import pandas as pd
+from datasets import load_dataset, concatenate_datasets, Dataset
 from transformers import AutoTokenizer
 
 class LyricsDataset():
-    def __init__(self, config, artist, dataset_id):
+    def __init__(self, config, filter_field, dataset_id):
         self.config = config
-        self.artist = artist
+        self.filter_field = filter_field
         self.dataset_id = dataset_id
         self.dataset_zip = self.config["dataset_zip"][self.dataset_id]
         self.tokenizer = AutoTokenizer.from_pretrained(self.config["model"])
         self.tokenizer.pad_token = self.tokenizer.eos_token
         self.files_to_delete = ["(Scriptonite)", "BTS", "Damso", "Genius English Translations", "Genius Romanizations", "JuL", "Nekfeu", "Oxxxymiron"]
         self.files_to_multiartist = ["Eminem10", "Justin Bieber10"]
+        # TODO: Remove this
         #self.files_to_multiartist = ["50 Cent", "Imagine Dragons", "Justin Bieber", "Taylor Swift", "Queen", "Lil Peep", "Arctic Monkeys", "The Notorious B.I.G.", "Radiohead", "Mac Miller"]
         self.dataset=""
 
@@ -22,6 +23,7 @@ class LyricsDataset():
         archive_name=re.sub(r'^.*?/', '',self.dataset_zip)
         archive_name=re.sub(r'.zip','',archive_name)
         self.dataset_dir = archive_name
+        print(archive_name)
 
         # Check wether selected database matches downloaded database
         if not os.path.exists(os.path.join(config["dataset_path"],self.dataset_dir)) or not os.listdir(os.path.join(config["dataset_path"],self.dataset_dir)):
@@ -44,58 +46,73 @@ class LyricsDataset():
     def load_dataset_single_artist(self):
         """Loads a dataset from a specific artist and stores its cleaned version"""
         if self.dataset_id == 'genious-lyrics':
-            csv_path = os.path.join(self.config["base_dir"], self.config["dataset_path"], self.dataset_dir, self.artist + ".csv")
+            csv_path = os.path.join(self.config["base_dir"], self.config["dataset_path"], self.dataset_dir, self.filter_field + ".csv")
             csvFile = load_dataset("csv", data_files=csv_path, split="train")
             csvFile = csvFile.map(self.__preprocess_lyrics)
             self.dataset = csvFile.select_columns("lyrics").train_test_split(test_size=0.1)
         
         elif self.dataset_id == '79-musical-genres':
             pass
-            # artists_csv_path = os.path.join(self.config["base_dir"], self.config["dataset_path"], self.dataset_dir + "/artists-data.csv")
-            # lyrics_csv_path = os.path.join(self.config["base_dir"], self.config["dataset_path"], self.dataset_dir + "/lyrics-data.csv")
-   
-            # artistsCsvFile = load_dataset("csv", data_files=artists_csv_path, split="train")
-            # lyricsCsvFile = load_dataset("csv", data_files=lyrics_csv_path, split="train")
-            # csvFile = lyricsCsvFile.merge(artistsCsvFile[['Artist', 'Genre', 'Link']], left_on='ALink', right_on='Link', how='inner')
-            # csvFile = csvFile.drop(columns=['ALink','SLink','Idiom','Link'])
-            # csvFile = csvFile.map(self.__preprocess_lyrics)
 
     def load_dataset_multiple_artists(self):
         """Loads several datasets from different artists and stores its cleaned version"""
-        folder_path = os.path.join(self.config["base_dir"], self.config["dataset_path"], self.dataset_dir)
-        csv_files = [filename for filename in os.listdir(folder_path) if filename.endswith(".csv")]
-        csv_files_to_keep = [filename for filename in csv_files if any(artist in filename for artist in self.files_to_multiartist)]
-        print("csv_files: ", csv_files)
-        print("csv_files_to_keep: ", csv_files_to_keep)
-        datasets = []
+        if self.dataset_id == 'genious-lyrics':
+            folder_path = os.path.join(self.config["base_dir"], self.config["dataset_path"], self.dataset_dir)
+            csv_files = [filename for filename in os.listdir(folder_path) if filename.endswith(".csv")]
+            csv_files_to_keep = [filename for filename in csv_files if any(artist in filename for artist in self.files_to_multiartist)]
+            print("csv_files: ", csv_files)
+            print("csv_files_to_keep: ", csv_files_to_keep)
+            datasets = []
 
-        # Append preprocess selected multiple artists datasets
-        for file in csv_files_to_keep:
-            print("FILE: ", file)
-            csv_path = os.path.join(self.config["base_dir"], self.config["dataset_path"], self.dataset_dir, file)
-            csvFile = load_dataset("csv", data_files=csv_path, split="train")
-            csvFile = csvFile.filter(lambda row: row['lyrics'] is not None and row['artist'] is not None)
-            csvFile = csvFile.map(self.__preprocess_lyrics_multiple_artists)
-            csvFile = csvFile.select_columns("lyrics")
-            datasets.append(csvFile)
+            # Append preprocess selected multiple artists datasets
+            for file in csv_files_to_keep:
+                print("FILE: ", file)
+                csv_path = os.path.join(self.config["base_dir"], self.config["dataset_path"], self.dataset_dir, file)
+                csvFile = load_dataset("csv", data_files=csv_path, split="train")
+                csvFile = csvFile.filter(lambda row: row['lyrics'] is not None and row['artist'] is not None)
+                csvFile = csvFile.map(self.__preprocess_lyrics_multiple_artists)
+                csvFile = csvFile.select_columns("lyrics")
+                datasets.append(csvFile)
+            
+            # Concatenate datasets and store it internally
+            self.dataset = concatenate_datasets(datasets)
+            self.dataset = self.dataset.train_test_split(test_size=0.1)
+            print("combined_dataset: ", self.dataset)
         
-        # Concatenate datasets and store it internally
-        self.dataset = concatenate_datasets(datasets)
-        print("combined_dataset: ", self.dataset)
-        self.dataset = self.dataset.train_test_split(test_size=0.1)
-        print("combined_dataset: ", self.dataset)
+        elif self.dataset_id == '79-musical-genres':
+            artists_csv_path = os.path.join(self.config["base_dir"], self.config["dataset_path"], self.dataset_dir + "/artists-data.csv")
+            lyrics_csv_path = os.path.join(self.config["base_dir"], self.config["dataset_path"], self.dataset_dir + "/lyrics-data.csv")
+            artistsCsvFile = pd.read_csv(artists_csv_path)
+            lyricsCsvFile = pd.read_csv(lyrics_csv_path)
+            
+            # Merge both databases
+            csvFile = lyricsCsvFile.merge(artistsCsvFile[['Artist', 'Genres', 'Popularity', 'Link']], left_on='ALink', right_on='Link', how='inner')
+            csvFile = self.__preprocess_lyrics_multiple_artists(csvFile)
+            self.dataset = Dataset.from_pandas(csvFile).select_columns("Lyric").train_test_split(test_size=0.1)
+            print("dataset: ", self.dataset)
 
     def tokenize(self, element):
         """Tokenizes a loaded dataset containing a lyrics section"""
         context_length = 128
-        outputs = self.tokenizer(
-            element["lyrics"],
-            truncation=True,
-            max_length=context_length,
-            return_overflowing_tokens=True,
-            return_length=True,
-        )
         input_batch = []
+
+        if self.dataset_id == 'genious-lyrics':
+            outputs = self.tokenizer(
+                element["lyrics"],
+                truncation=True,
+                max_length=context_length,
+                return_overflowing_tokens=True,
+                return_length=True,
+            )
+        elif self.dataset_id == '79-musical-genres':
+            outputs = self.tokenizer(
+                element["Lyric"],
+                truncation=True,
+                max_length=context_length,
+                return_overflowing_tokens=True,
+                return_length=True,
+            )
+
         for length, input_ids in zip(outputs["length"], outputs["input_ids"]):
             if length == context_length:
                 input_batch.append(input_ids)
@@ -115,14 +132,21 @@ class LyricsDataset():
             # res = re.search(r'[-+]?(\d+)KEmbed', data['lyrics'])
         elif self.dataset_id == '79-musical-genres':
             pass
-            # data['lyrics'] = data[data['Idiom']=='ENGLISH']
-            # print(data)
+
         return data
     
     def __preprocess_lyrics_multiple_artists(self, data):
         """Preprocesses multiple artists lyrics by removing first line and text between square brakets"""
-        data = self.__preprocess_lyrics(data)
-        data['lyrics'] =  data['artist'] + ": " + data['lyrics']
+        if self.dataset_id == 'genious-lyrics':
+            data = self.__preprocess_lyrics(data)
+            data['lyrics'] =  data['artist'] + ": " + data['lyrics']
+        elif self.dataset_id == '79-musical-genres':
+            # Select only english songs
+            data = data[data['language']=='en']
+            data = data[(data['Genres'].str.contains(self.filter_field, case=False, na=False)) & (data['Popularity'] > 5)]
+            data = data.drop(columns=['ALink','SLink','Link','Popularity'])
+            #TODO: Remove this
+            data = data.drop(data.index[2:-1])
 
         return data
 
--- gpt2-model/main.py
+++ gpt2-model/main.py
@@ -55,15 +55,18 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog='Lyrics generator', description="Trains a lyrics generation model based on GPT-2 architecture.")
     parser.add_argument("-ts", "--trainSingleArtist", dest='train_single_artist', type=str, help="Prepare the dataset of the choosen artist and train the model")
     parser.add_argument("-tm", "--trainMultipleArtists", dest='train_multiple_artist', action="store_true", help="Prepare the dataset of all the artists and train the model")
+    parser.add_argument("-tg", "--trainGenre", dest='train_genre', type=str, help="Pass the genre to train the model with songs of that genre")
     parser.add_argument("-gs", "--generateSingleArtist", dest='generate_single_artist', type=str, help="Pass the artist name to generate lyrics. Use the same name you used to train it.")
-    parser.add_argument("-gm", "--generateMultipleArtist", dest='generate_multiple_artist',type=str, help="Pass the artist name to generate lyrics with the model trained with multiple artistrs. Use the same name you used to train it.")
-    parser.add_argument("-d", "--datasetSelection", dest='dataset_selection', choices=["genious-lyrics","79-musical-genres"], help="Offers dataset selection between two choices")
+    parser.add_argument("-gm", "--generateMultipleArtist", dest='generate_multiple_artist',type=str, help="Pass the artist name to generate lyrics with the model trained with multiple artists. Use the same name you used to train it.")
+    parser.add_argument("-gg", "--generateMultipleArtistGenre", dest='generate_multiple_artist_genre',type=str, help="Pass the artist name to generate lyrics with the model trained with multiple artists genre. Use the same name you used to train it.")
+    parser.add_argument("-ds", "--datasetSelection", dest='dataset_selection', choices=["genious-lyrics","79-musical-genres"], help="Offers dataset selection between two choices")
     args = parser.parse_args()
 
     # Set default arguments
     if args.dataset_selection == None:
         args.dataset_selection="genious-lyrics"
 
+    # Training options
     if(args.train_single_artist):
         print("Selected single-artist training: ", args.train_single_artist)
         lyrics_dataset = LyricsDataset(config, args.train_single_artist, args.dataset_selection)
@@ -72,23 +75,30 @@ if __name__ == "__main__":
             lyrics_dataset.tokenize, batched=True, remove_columns=lyrics_dataset.dataset["train"].column_names
         )
         train_model(lyrics_dataset, tokenized_dataset, args.train_single_artist)
-    
     elif(args.train_multiple_artist):
         print("Selected multi-artist tranining")
-        lyrics_dataset = LyricsDataset(config, args.train_single_artist, args.dataset_selection)
+        lyrics_dataset = LyricsDataset(config, "multipleArtists", args.dataset_selection)
         lyrics_dataset.load_dataset_multiple_artists()
         tokenized_dataset = lyrics_dataset.dataset.map(
             lyrics_dataset.tokenize, batched=True, remove_columns=lyrics_dataset.dataset["train"].column_names
         )
         train_model(lyrics_dataset, tokenized_dataset, "multipleArtists")
+    elif(args.train_genre):
+        print("Selected genre tranining")
+        lyrics_dataset = LyricsDataset(config, args.train_genre, "79-musical-genres")
+        lyrics_dataset.load_dataset_multiple_artists()
+        tokenized_dataset = lyrics_dataset.dataset.map(
+            lyrics_dataset.tokenize, batched=True, remove_columns=lyrics_dataset.dataset["train"].column_names
+        )
+        train_model(lyrics_dataset, tokenized_dataset, "multipleArtistsGenre")
 
+    # Generation options
     if(args.generate_single_artist):
         print("Selected single-artist generation: ", args.generate_single_artist)
         lyrics_generator_params = LyricsGeneratorParams
         lyrics_generator_params.max_length = 10
         lyrics_generator = LyricsGenerator(config, args.generate_single_artist, lyrics_generator_params)
         lyrics_generator.generate_lyrics(initial_prompt="My name is")
-    
     elif(args.generate_multiple_artist):
         print("Selected multiple-artist generation")
         lyrics_generator_params = LyricsGeneratorParams
@@ -97,5 +107,12 @@ if __name__ == "__main__":
         lyrics_generator = LyricsGenerator(config, "multipleArtists", lyrics_generator_params)
         lyrics_generator.generate_lyrics(args.generate_multiple_artist + ': ' + initial_prompt)
 
+    elif(args.generate_multiple_artist_genre):
+            print("Selected multiple-artist genre generation")
+            lyrics_generator_params = LyricsGeneratorParams
+            lyrics_generator_params.max_length = 20
+            initial_prompt="You are"
+            lyrics_generator = LyricsGenerator(config, "multipleArtistsGenre", lyrics_generator_params)
+            lyrics_generator.generate_lyrics(args.generate_multiple_artist_genre + ': ' + initial_prompt)
 
 
